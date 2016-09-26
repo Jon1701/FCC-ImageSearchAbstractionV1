247f6625fcde8e06809573af36e9c230706570e8
Jon Balon <development@jonbalon.com>
2016-09-26 01:07:57 -0400

    Started preliminary work on <Results/>

--- src/js/App/Results.jsx
+++ src/js/App/Results.jsx
@@ -1,5 +1,6 @@
 // React
 import React from 'react';
+import classNames from 'classnames';
 
 export default class Results extends React.Component {
 
@@ -10,9 +11,44 @@ export default class Results extends React.Component {
 
   // Component render.
   render() {
+
+    // Component classes.
+    var myClasses = classNames({
+      'hidden': this.props.results == null  // Hide if no search was made.
+    });
+
+    var html;
+
+    if (this.props.results) {
+
+      // Unwrap this.props.results.
+      var results = this.props.results.results;
+      var num_results = this.props.results.num_results;
+      var page = this.props.results.page;
+      var query = this.props.results.q;
+
+      html = results.map((result, idx, arr) => {
+        return (
+          <div key={idx} className="panel boxshadow clear-margin col-xs-6 col-sm-6 col-md-3 col-lg-3">
+            <a href={result.link} target="blank">
+              <img src={result.link} className="img-responsive thumbnail"/>
+            </a>
+          </div>
+        );
+      });
+    };
+
     return (
-      <div className="panel boxshadow">
-        Results
+      <div className={myClasses}>
+
+        <div className="panel boxshadow text-center">
+          Displaying {num_results} matching images.
+        </div>
+
+        <div>
+          {html}
+        </div>
+
       </div>
     );
   };
